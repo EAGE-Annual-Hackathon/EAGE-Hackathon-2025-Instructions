da0bedda2e83e5f191696958945c596d0abbc5ca
Lukas Mosser <lukas.mosser@gmail.com>
2025-05-17 08:12:25 +0200

    Add CODEOWNERS, gitattributes and license headers

--- hello_world_mcp/server.py
+++ hello_world_mcp/server.py
@@ -1,3 +1,19 @@
+"""
+This file is part of the EAGE Hackathon 2025 project.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License in the LICENSE file at the
+root of this repository or at
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
 from fastmcp import FastMCP, Image
 from starlette.requests import Request
 from starlette.responses import JSONResponse
