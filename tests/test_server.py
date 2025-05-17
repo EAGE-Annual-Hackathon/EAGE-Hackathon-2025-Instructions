0eb01edf924b7f58d7aae0351c186820eafa1fa2
Lukas Mosser <lukas.mosser@gmail.com>
2025-05-17 09:12:44 +0200

    fix tests by installing package

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,3 +1,8 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from hello_world_mcp.server import hello_world
 
 def test_hello_world():
